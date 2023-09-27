c92157dda159c2db52ff900bfe015a1787b6251f
Urvish <92674497+UrvishP4503@users.noreply.github.com>
2023-09-27 10:58:03 +0530

    pr3 almost complete.

--- pr3/main.cpp
+++ pr3/main.cpp
@@ -1,12 +1,24 @@
 #include "max_heap.hpp"
 #include <iostream>
+#include <vector>
 
 int main(int argc, char const *argv[]) {
-    Max_heap<int> test1 = {1, 3, 9, 4};
-    Max_heap<int> test2 = Max_heap(3);
-    int arr[] = {1, 23, 4, 5};
-    test2.add(arr, 4);
-    test1.print();
+    // 17, 16, 13, 14, 7, 9, 11, 5, 12, 8, 1, 4, 3, 6, 10
+    // Max_heap<int> test1 = {50, 54, 53, 52, 55};
+    // int arr[] = {1, 23, 4, 3};
+    // test1.add(arr, 4);
+    // test1.print();
+
+    std::vector<int> arr = {9, 4, 7, 1, -2, 6, 5};
+    Max_heap<int> test2 = arr;
+    test2.print();
+
+    for (int i = 5 / 2; i >= 0; i--) {
+        test2.heapify(i);
+    }
+
+    test2.print();
+    test2.heap_sort();
     test2.print();
     return 0;
 }
--- pr3/max_heap.hpp
+++ pr3/max_heap.hpp
@@ -11,82 +11,116 @@ private:
     int size{0};
 
 public:
-    inline Max_heap() {}
-    Max_heap(T element);
-    Max_heap(std::initializer_list<T> initial_list);
-    void add(T element);
-    void add(T elements[], int length);
+    Max_heap(std::vector<int> &vec);
+    void heapify(int i);
+    void heap_sort();
+    // inline Max_heap() {}
+    // Max_heap(std::initializer_list<T> initial_list);
+    // void add(T elements[], int length);
     void print();
 };
 
 template <class T>
-Max_heap<T>::Max_heap(T element) {
-    this->heap.push_back(element);
-    this->size++;
-}
-
-template <typename T>
-Max_heap<T>::Max_heap(std::initializer_list<T> initial_list) {
-    for (const T &element : initial_list) {
+Max_heap<T>::Max_heap(std::vector<int> &vec) {
+    for (const T &element : vec) {
         this->size++;
         this->heap.push_back(element);
 
         int child = this->size;
         while (child > 1) {
             int parent = child / 2;
-
             if (this->heap[child - 1] > this->heap[parent - 1]) {
                 std::swap(this->heap[child - 1], this->heap[parent - 1]);
+                child = parent;
             } else {
                 break;
             }
-
-            child = parent;
         }
     }
 }
 
-// template <class T>
-// void add(T element) {
-//     int child = size++;
-//     while (child > 1) {
-//         int parent = child / 2;
+template <class T>
+void Max_heap<T>::heapify(int i) {
 
-//         if (heap[child - 1] > heap[parent - 1]) {
-//             std::swap(heap[child - 1], heap[parent - 1]);
-//         } else {
-//             break;
+    int largest = i;
+    int left = (2 * i) + 1;
+    int right = (2 * i) + 2;
+
+    if (left < this->size && this->heap[largest] < this->heap[left]) {
+        largest = left;
+    }
+
+    if (right < this->size && this->heap[largest] < this->heap[right]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        std::swap(this->heap[i], this->heap[largest]);
+        this->heapify(largest);
+    }
+}
+
+template <class T>
+void Max_heap<T>::heap_sort() {
+    if (this->size <= 1) {
+        return;
+    }
+
+    int temp = this->size;
+    int len = this->size;
+    while (len >= 1) {
+        int temp = len - 1;
+        std::swap(this->heap[0], this->heap[temp]);
+        len--;
+        this->size--;
+        this->heapify(0);
+    }
+    this->size = temp;
+}
+
+// template <typename T>
+// Max_heap<T>::Max_heap(std::initializer_list<T> initial_list) {
+//     for (const T &element : initial_list) {
+//         this->size++;
+//         this->heap.push_back(element);
+
+//         int child = this->size;
+//         while (child > 1) {
+//             int parent = child / 2;
+
+//             if (this->heap[child - 1] > this->heap[parent - 1]) {
+//                 std::swap(this->heap[child - 1], this->heap[parent - 1]);
+//                 child = parent;
+//             } else {
+//                 break;
+//             }
 //         }
+//     }
+// }
 
-//         child = parent;
+// template <class T>
+// void Max_heap<T>::add(T element[], int length) {
+//     for (int i = 0; i < length; i++) {
+//         this->size++;
+//         this->heap.push_back(element[i]);
+
+//         int child = this->size;
+//         while (child > 1) {
+//             int parent = child / 2;
+
+//             if (this->heap[child - 1] > this->heap[parent - 1]) {
+//                 std::swap(this->heap[child - 1], this->heap[parent - 1]);
+//             } else {
+//                 break;
+//             }
+//         }
 //     }
 // }
 
 template <class T>
-void Max_heap<T>::add(T element[], int length) {
-    for (int i = 0; i < length; i++) {
-        this->size++;
-        this->heap.push_back(element[i]);
-
-        int child = this->size;
-        while (child > 1) {
-            int parent = child / 2;
-
-            if (this->heap[child - 1] > this->heap[parent - 1]) {
-                std::swap(this->heap[child - 1], this->heap[parent - 1]);
-            } else {
-                break;
-            }
-
-            child = parent;
-        }
-    }
-}
-
-template <class T>
 void Max_heap<T>::print() {
-    for (const T &element : this->heap) {
-        std::cout << element << " ";
+    for (int i = 0; i < this->size; i++) {
+        std::cout << this->heap.at(i) << " ";
     }
     std::cout << std::endl;
 }

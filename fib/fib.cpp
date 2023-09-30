c072a2a48950b22074569f2dee6a503cd1409906
Urvish <92674497+UrvishP4503@users.noreply.github.com>
2023-09-30 23:34:03 +0530

    trying out functional programming

--- fib/fib.cpp
+++ fib/fib.cpp
@@ -1,23 +1,25 @@
+#include <functional>
 #include <iostream>
 #include <unordered_map>
 
-double fib(int n, std::unordered_map<int, int> &memo) {
-    if (memo.find(n) != memo.end()) {
-        return memo[n];
-    } else if (n <= 2) {
-        return 1;
-    } else {
-        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-        return memo[n];
-    }
-}
+// double fib(int n, std::unordered_map<int, double> &memo) {
+//     if (memo.find(n) != memo.end()) {
+//         return memo[n];
+//     } else if (n <= 2) {
+//         return 1;
+//     } else {
+//         memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+//         return memo[n];
+//     }
+// }
 
 int main(int argc, char const *argv[]) {
-    std::unordered_map<int, int> temp;
+    static std::unordered_map<int, double> memo;
 
-    auto i = fib(23, temp);
-
-    std::cout << i;
-
-    return 0;
+    const std::function<double(int)> fib = [&](int n) -> int {
+        return (memo.find(n) != memo.end()) ? memo[n]
+               : (n <= 2)                   ? 1
+                                            : (memo[n] = fib(n - 1) + fib(n - 2));
+    };
+    std::cout << fib(3);
 }
